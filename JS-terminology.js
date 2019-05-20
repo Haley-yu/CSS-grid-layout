abcb09ebf3837557c02409e1a69af03def6751c8
xiaoxiaoha777 <50051465+xiaoxiaoha777@users.noreply.github.com>
2019-05-20 23:10:31 +0800

    JS-terminology.js

---
+++ JS-terminology.js
@@ -0,0 +1,34 @@
+// function declaration
+function newFunction(){
+
+}
+// function expression
+var newFunction = function(){
+
+};
+
+// expression
+1+3;
+var a =2;
+return true;
+
+// calling or invoking a function
+alert();
+newFunction(param1,param2);
+
+// assign a variable
+var a =true;
+
+// function vs methon
+function thisIsAFunction(){
+
+}
+
+var obj ={
+	thisIsAMethon:function(){
+
+	}
+}
+
+thisIsAFunction()
+obj.thisIsAMethon()
